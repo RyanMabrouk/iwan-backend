089e1fe94895d28f461570cb16993e6f6868e236
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-08 20:28:40 +0100

    feat: Sort wishlist items by book creation date in WishlistRepository

--- src/app/modules/users/modules/wishlist/infrastructure/repository.ts
+++ src/app/modules/users/modules/wishlist/infrastructure/repository.ts
@@ -29,6 +29,7 @@ export class WishlistRepository
         .selectFrom('wishlists')
         .where('wishlists.user_id', '=', user_id)
         .innerJoin('books', 'books.id', 'wishlists.book_id')
+        .orderBy('books.created_at', 'desc')
         .$if(!!query.filters, (q) =>
           q.where((e) =>
             e.and(
