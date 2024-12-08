00cd87b72728b9a55f2cb50f2f6516af76cef4bf
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-08 22:05:21 +0100

    feat: Add UsersModule import to ReviewsModule for user-related functionalities

--- src/app/modules/books/modules/reviews/reviews.module.ts
+++ src/app/modules/books/modules/reviews/reviews.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/app/database/database.module';
 import { ReviewController } from './reviews.controller';
 import { ReviewService } from './reviews.service';
+import { UsersModule } from 'src/app/modules/users/user.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [ReviewController],
   providers: [ReviewRepository, ReviewService],
   exports: [ReviewRepository, ReviewService],
