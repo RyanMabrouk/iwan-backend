35b45d1929b8355083bbd17ca85647dae303d58d
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-16 18:37:27 +0100

    feat: Add OfferModule to orders module imports

--- src/app/modules/payment/orders/orders.module.ts
+++ src/app/modules/payment/orders/orders.module.ts
@@ -7,9 +7,16 @@ import { DatabaseModule } from 'src/app/database/database.module';
 import { OrderProductModule } from './modules/order_products/OrderProduct.module';
 import { BooksModule } from '../../books/book.module';
 import { UsersModule } from '../../users/user.module';
+import { OfferModule } from '../../offers/offer.module';
 
 @Module({
-  imports: [DatabaseModule, OrderProductModule, BooksModule, UsersModule],
+  imports: [
+    DatabaseModule,
+    OrderProductModule,
+    BooksModule,
+    UsersModule,
+    OfferModule,
+  ],
   controllers: [OrdersController],
   providers: [OrderFactory, OrderRepository, OrdersService],
   exports: [OrderFactory, OrderRepository, OrdersService, OrderProductModule],
