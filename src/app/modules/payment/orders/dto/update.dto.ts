9196b5171aed8a59d4749f1aea322f84e2fd225c
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-16 18:33:27 +0100

    feat: Update order DTO to exclude only payment_method from CreateOrder

--- src/app/modules/payment/orders/dto/update.dto.ts
+++ src/app/modules/payment/orders/dto/update.dto.ts
@@ -2,5 +2,5 @@ import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateOrder } from './create.dto';
 
 export class UpdateOrderDto extends PartialType(
-  OmitType(CreateOrder, ['payment_method', 'books']),
+  OmitType(CreateOrder, ['payment_method']),
 ) {}
